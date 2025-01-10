19413db048e43bbf53cc6a27a524208622e458c8
Rithick Dharma Raj <84231703+RithickDharmaRaj-darkCoder@users.noreply.github.com>
2025-01-10 17:19:21 +0530

    Create Text Field.dart

---
+++ Text Field.dart
@@ -0,0 +1,70 @@
+// Text Field
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(100),
+                      bottomRight: Radius.circular(100)),
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://leadgenapp.io/wp-content/uploads/2022/12/shutterstock_1100033681-min-1-Edited.png"),
+                      fit: BoxFit.cover)),
+            ),
+            Text("Feedback Form",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    fontStyle: FontStyle.italic)),
+            Container(
+              height: 370,
+              width: double.infinity,
+              //color: Colors.green,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        hintText: "Enter Your Name",
+                        labelText: "Your Name",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.message),
+                        hintText: "Enter Your WhatsApp Number",
+                        labelText: "WhatsApp Number",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15))),
+                  )
+                ]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
