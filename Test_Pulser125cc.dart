a6c2e6e96eb6da9fea53d52dcc9f63d9f765d8e2
Rithick Dharma Raj <84231703+RithickDharmaRaj-darkCoder@users.noreply.github.com>
2025-01-10 13:00:02 +0530

    Final banner, Scroll View added

--- Test_Pulser125cc.dart
+++ Test_Pulser125cc.dart
@@ -17,51 +17,74 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              Image.network(
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSufrSeMBfKEh3EUHNjrPdMZDLO2193mKr3OQ&s",
-                height: 80,
-              ),
-              Image.network(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.network(
+                  // logo
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSufrSeMBfKEh3EUHNjrPdMZDLO2193mKr3OQ&s",
+                  height: 80,
+                ),
+                Image.network(
+                  // Red pic
                   "https://5.imimg.com/data5/SELLER/Default/2023/6/319337975/ZF/LI/NX/4954/bajaj-pulsar-125cc-1000x1000.png",
-                  height: 180),
-              Text(
-                "Pulsar 125 CC",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Specification",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 100,
-                width: double.infinity,
-                //color: Colors.grey,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      buildSpecContainer(
-                          "https://eauto.co.in/cdn/shop/products/mukut-front-disc-brake-plate-bajaj-pulsar-135-664.jpg?v=1631369845",
-                          "Disk"),
-                      buildSpecContainer(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc5odOA7jbbDIk8fYS_wmqOsJAxU-eXVWROw&s",
-                          "125 CC"),
-                      buildSpecContainer(
-                          "https://i.pinimg.com/564x/b4/b5/ec/b4b5eca714876d75017a2666843c3c91.jpg",
-                          "45 PS"),
-                      buildSpecContainer(
-                          "https://i.pinimg.com/564x/09/2e/d3/092ed39174841d577c91ac844fd91ea0.jpg",
-                          "45.5 KM")
-                    ]),
-              )
-            ],
+                  height: 200,
+                ),
+                Text(
+                  "Pulsar 125 CC",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Specification",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.redAccent),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  //color: Colors.grey,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        buildSpecContainer(
+                            "https://eauto.co.in/cdn/shop/products/mukut-front-disc-brake-plate-bajaj-pulsar-135-664.jpg?v=1631369845",
+                            "Disk"),
+                        buildSpecContainer(
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc5odOA7jbbDIk8fYS_wmqOsJAxU-eXVWROw&s",
+                            "125 CC"),
+                        buildSpecContainer(
+                            "https://i.pinimg.com/564x/b4/b5/ec/b4b5eca714876d75017a2666843c3c91.jpg",
+                            "45 PS"),
+                        buildSpecContainer(
+                            "https://i.pinimg.com/564x/09/2e/d3/092ed39174841d577c91ac844fd91ea0.jpg",
+                            "45.5 KM")
+                      ]),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.yellowAccent,
+                      borderRadius: BorderRadius.all(Radius.circular(25)),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://static.autox.com/uploads/2019/09/Bajaj-Pulsar-125-Motion.jpg"),
+                          fit: BoxFit.cover)),
+                )
+              ],
+            ),
           ),
         ),
       ),
